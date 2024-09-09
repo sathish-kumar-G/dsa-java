40c274cdd48c4af8beaa4b1a096da5b21ab2ff3b
sathish <sathishkumar.ganesan1997@gmail.com>
2024-09-09 17:50:13 +0530

    added-pattern1

---
+++ java-dsa/src/main/java/org/learn/patterns/Pattern1.java
@@ -0,0 +1,20 @@
+package org.learn.patterns;
+
+public class Pattern1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//Result
+//****
+//****
+//****
+//****
