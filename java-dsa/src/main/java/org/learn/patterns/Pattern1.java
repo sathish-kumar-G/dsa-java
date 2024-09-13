7bd610792a89e3c6b12a9d92ed1831634e6b091a
sathish <sathishkumar.ganesan1997@gmail.com>
2024-09-13 16:15:25 +0530

    completed-pattern-8

--- java-dsa/src/main/java/org/learn/patterns/Pattern1.java
+++ java-dsa/src/main/java/org/learn/patterns/Pattern1.java
@@ -4,6 +4,20 @@ public class Pattern1 {
 
     public static void main(String[] args) {
 
+        int n = 5;
+//        pattern1(n);
+//        pattern2(n);
+//        pattern3(n);
+//        pattern4(n);
+//        pattern5(n);
+//        pattern6(n);
+//        pattern7(n);
+//        pattern8(n);
+        pattern9(n);
+
+    }
+
+    private static void pattern1(int n) {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 System.out.print("* ");
@@ -11,10 +25,140 @@ public class Pattern1 {
             System.out.println();
         }
     }
+
+    private static void pattern2(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void pattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void pattern4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void pattern5(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void pattern6(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void pattern7(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void pattern8(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * n - 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void pattern9(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
 
-//Result
+//Pattern1 - Result
 //****
 //****
 //****
 //****
+
+//Pattern2
+//*
+//* *
+//* * *
+//* * * *
+//* * * * *
+
+//Pattern3
+//1
+//1 2
+//1 2 3
+//1 2 3 4
+//1 2 3 4 5
+
+//Pattern4
+//1
+//2 2
+//4 4 4 4
+//5 5 5 5 5
+
+//Pattern5
+//* * * * *
+//* * * *
+//* * *
+//* *
+//*
+
+//Pattern6
+//1 2 3 4 5
+//1 2 3 4
+//1 2 3
+//1 2
+//1
+
+//Pattern7
+//    *
+//   ***
+//  *****
+// *******
+//*********
+
+//Pattern8
+//    *
+//   ***
+//  *****
+// *******
+//*********
+
